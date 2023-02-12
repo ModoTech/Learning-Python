cc96b6c10ad066a8b7b2597f3abd397f69a56e78
unknown <kevinleen.kl@gmail.com>
2023-02-12 20:05:41 +0300

    removed Twilio SSD

--- send_message.py
+++ send_message.py
@@ -1,4 +1,4 @@
 import os
 
-account_sid = os.environ['ACfbdf472427d988757c405f25960593ac']
-auth_token = os.environ['882ee4ee0a21a65278953e9c42031dba']
+account_sid = os.environ['']
+auth_token = os.environ['']
