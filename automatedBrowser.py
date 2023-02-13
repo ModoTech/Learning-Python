db597b7767244981978cd718a57c40a8171b6106
unknown <kevinleen.kl@gmail.com>
2023-02-13 17:23:18 +0300

    Student's portal autologin using webDriver

---
+++ automatedBrowser.py
@@ -0,0 +1,8 @@
+#this program automatically logins to a web page passing
+#the login authentication test
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+#Portal credentials
+userID = "BSIT/0109/2019"
